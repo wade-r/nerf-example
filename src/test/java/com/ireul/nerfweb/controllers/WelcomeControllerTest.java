10f40093fa0519395f388e89da5c5cea5962dd7c
Ryan Wade <ryan.wade@protonmail.com>
2017-06-02 04:26:04 +0800

    update nerf version; add tests

---
+++ src/test/java/com/ireul/nerfweb/controllers/WelcomeControllerTest.java
@@ -0,0 +1,28 @@
+package com.ireul.nerfweb.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ryan on 6/2/17.
+ */
+public class WelcomeControllerTest {
+
+    @Test
+    public void index() throws Exception {
+    }
+
+    @Test
+    public void users() throws Exception {
+    }
+
+    @Test
+    public void raise() throws Exception {
+    }
+
+    @Test
+    public void view() throws Exception {
+    }
+
+}
